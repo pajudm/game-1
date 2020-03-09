c0fdfbedcf03fee9d08622db35b124dbeef6dfef
jddupuy <dupuy.juan@gmail.com>
2020-03-09 19:09:28 -0400

    Added my stuff. Box Follow.

--- Assets/Scripts/BoxFollow.cs
+++ Assets/Scripts/BoxFollow.cs
@@ -4,20 +4,38 @@ using UnityEngine;
 
 public class BoxFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Speed Variable
     public float speed;
+
+    // Variables for Box Bullet
+    [SerializeField]
+    GameObject Bullet;
+
+    public float fireRate;
+    float nextFire;
+
 
     private Transform target;
 
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        nextFire = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        //transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        CheckIfTimeToFire();
+    }
 
+    void CheckIfTimeToFire()
+    {
+        if(Time.time > nextFire)
+        {
+            Instantiate(Bullet, transform.position, Quaternion.identity);
+            nextFire = Time.time + fireRate;
+        }
     }
 }
---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed;
+
+    Vector2 moveDirection;
+    Rigidbody2D rb;
+
+    PlayerMovement target; 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.FindObjectOfType<PlayerMovement>();
+        moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
+        rb.velocity = new Vector2 (moveDirection.x, moveDirection.y);
+        Destroy(gameObject, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
